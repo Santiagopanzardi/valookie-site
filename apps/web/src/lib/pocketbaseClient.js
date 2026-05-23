386f01d258fd8c61cb8410f8c746317bd789c3c2
Santiagopanzardi <valookie@email.com>
2026-05-23 17:43:51 +0200

    Conecta PocketBase en Fly.io y actualiza URL del cliente

--- apps/web/src/lib/pocketbaseClient.js
+++ apps/web/src/lib/pocketbaseClient.js
@@ -1,6 +1,6 @@
 import Pocketbase from 'pocketbase';
 
-const POCKETBASE_API_URL = "/hcgi/platform";
+const POCKETBASE_API_URL = import.meta.env.VITE_POCKETBASE_URL || "https://horizons.hostinger.com/20aebe59-367d-486d-b6c5-8fc8284b40e6/hcgi/platform";
 
 const pocketbaseClient = new Pocketbase(POCKETBASE_API_URL);
 
